d6fef1fe7b9524bd12565a42a5c27174584d31ef
busti <mbust+git@mailbox.org>
2020-08-06 00:40:11 +0200

    modify while true loop to timeout after 1 hour

--- reprepro/startup.sh
+++ reprepro/startup.sh
@@ -5,12 +5,12 @@ inotifywait -m /in -e create -e moved_to |
   while read path action file; do
     if [[ "$file" =~ .*deb$ ]]; then
       echo "found '$file' via '$action' in '$path'"
-      while [ 1 ]; do
+      for i in {1..3600}; do
         res=$( reprepro -b /out includedeb trusty "$path$file" 2>&1 )
         echo "reprepro returned: '$res'"
         if [[ $res =~ "error" ]]; then
           echo "retrying"
-          sleep 0.5
+          sleep 1
         else
           echo "successfully added package"
           echo "removing artifact: '$path$file'"
